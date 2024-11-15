65df70ff8b03c9dce13aeb44da77f7b5ab0e9603
joonsol <71648638+joonsol@users.noreply.github.com>
2024-11-15 14:10:21 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import Controller from './components/Controller'
+import Viewer from './components/Viewer'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
+      <section>
+        <Controller />
+      </section>
+    </div>
+  )
+}
+
+export default App
