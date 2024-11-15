d3bc17ff68c4c167c3944a1b270e760b2faf3ec8
joonsol <71648638+joonsol@users.noreply.github.com>
2024-11-15 10:13:35 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+function App() {
+
+  return (
+    <div>
+      {undefined}//(X)
+      {null}//(X)
+      {/* {obj}//error */}
+      <Main/>
+    </div>
+  )
+}
+
+export default App
