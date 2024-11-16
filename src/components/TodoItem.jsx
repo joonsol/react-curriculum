22642d1d9f668967f4574d1c85dc8d80d4b4d847
joonsol <71648638+joonsol@users.noreply.github.com>
2024-11-16 20:54:38 +0900

    Add files via upload

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,28 @@
+import "./TodoItem.css";
+
+const TodoItem = ({ id, isDone, content, date, onUpdate,onDelete }) => {
+
+  const onChangeCheckbox = () => {
+    onUpdate(id); // 클릭된 항목의 ID를 전달
+  };
+  const onClickDeleteButton = () => {
+    onDelete(id); // 클릭된 항목의 ID를 onDelete 함수로 전달
+  };
+
+  return (
+    <div className="TodoItem">
+      <input
+        onChange={onChangeCheckbox} // 체크박스 상태 변경 시 호출
+        readOnly
+        checked={isDone}           // 현재 isDone 상태 반영
+        type="checkbox"
+      />
+      <div className="content">{content}</div>            {/* 할 일 내용 */}
+      <div className="date">{new Date(date).toLocaleDateString()}</div> {/* 날짜 */}
+      <button  onClick={onClickDeleteButton}>삭제</button>                              {/* 삭제 버튼 */}
+    </div>
+  );
+};
+
+
+export default TodoItem;
