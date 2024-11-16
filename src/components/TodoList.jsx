2f72c3b7bca0a13916ad67e5baedb70f8f6a531f
joonsol <71648638+joonsol@users.noreply.github.com>
2024-11-16 20:21:21 +0900

    Add files via upload

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,18 @@
+import "./TodoList.css";
+import TodoItem from "./TodoItem";
+
+const TodoList = (params) => {
+  return (
+    <div className="TodoList">
+      <h4>Todo List 🌱</h4>
+      <input placeholder="검색어를 입력하세요" />
+      <div className="todos_wrapper">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
