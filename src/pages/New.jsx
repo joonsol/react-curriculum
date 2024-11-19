c1d0b7f9b3126264ae8a7700c3a64e22e34a0299
joonsol <71648638+joonsol@users.noreply.github.com>
2024-11-19 09:57:43 +0900

    Add files via upload

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,22 +1,11 @@
-import { useContext } from "react"
-import {DiaryDispatchContext} from "../App"
+
 import Editor from "../components/Editor"
 import Header from "../components/Header"
 import Button from "../components/Button"
-import { useNavigate } from "react-router-dom"
 const New = () => {
 
-  const {onCreate }=useContext(DiaryDispatchContext)
-  const nav =useNavigate()
 
-  const onSubmit =(input)=>{
-    onCreate(
-      input.createdDate.getTime(),
-      input.emotionId,
-      input.content
-    )
-    nav("/", {replace:true})
-  }
+
   return(
     <div>
       <Header
