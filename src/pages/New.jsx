e2a575eb3ade2d459fdde7af4f8f50f77891ce1b
joonsol <71648638+joonsol@users.noreply.github.com>
2024-11-19 11:54:33 +0900

    Add files via upload

---
+++ src/pages/New.jsx
@@ -0,0 +1,32 @@
+import { useContext ,useState} from "react"
+import {DiaryDispatchContext} from "../App"
+import Editor from "../components/Editor"
+import Header from "../components/Header"
+import Button from "../components/Button"
+import { useNavigate } from "react-router-dom"
+const New = () => {
+
+
+
+  const {onCreate }=useContext(DiaryDispatchContext)
+  const nav =useNavigate()
+
+  const onSubmit =(input)=>{
+    onCreate(
+      input.createdDate.getTime(),
+      input.emotionId,
+      input.content
+    )
+    nav("/", {replace:true})
+  }
+  return(
+    <div>
+      <Header
+      leftChild={<Button text={"< 뒤로가기"} onClick={()=>nav(-1)}/>}
+      title="새 일기 쓰기"
+      />
+      <Editor onSubmit={onSubmit}/>
+    </div>
+  )
+}
+export default  New;
